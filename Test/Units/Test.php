69780c1d8348d14d4709b4d9c70681991d55a772
Alexandre FONTFREYDE <afontfreyde@stsweb.fr>
2015-12-09 09:33:10 +0000

    fix compatibility with atoum 2.4.0

--- Test/Units/Test.php
+++ Test/Units/Test.php
@@ -24,9 +24,9 @@ abstract class Test extends atoum\test
      */
     public function __construct(atoum\adapter $adapter = null, atoum\annotations\extractor $annotationExtractor = null, atoum\asserter\generator $asserterGenerator = null, atoum\test\assertion\manager $assertionManager = null, \closure $reflectionClassFactory = null)
     {
-        $this->setTestNamespace('Tests\Units');
-
         parent::__construct($adapter, $annotationExtractor, $asserterGenerator, $assertionManager, $reflectionClassFactory);
+
+        $this->setTestNamespace('Tests\Units');
     }
 
     /**
